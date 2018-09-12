153e3a3f1b78091bfee7d876e0fb71bfa15aba24
katzy687 <katzy687@gmail.com>
2018-09-12 13:09:29 +0300

    updated allow win rm

--- allow_winrm.ps1
+++ allow_winrm.ps1
@@ -1,5 +1,6 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
 Enable-PSRemoting -SkipNetworkProfileCheck -Force
 winrm set winrm/config/client/auth '@{Basic="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-Test-WSMan
