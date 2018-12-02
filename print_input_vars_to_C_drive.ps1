e76b2eda4ed92c2a93fd6a9f7bd630b56227d1c5
katzy687 <katzy687@gmail.com>
2018-12-02 13:28:56 +0200

    updated

--- print_input_vars_to_C_drive.ps1
+++ print_input_vars_to_C_drive.ps1
@@ -1,9 +1,9 @@
 $localpass = $env:local_password
 $dcpass = $env:dc_password
 
-$dir_name = "test-dir"
+$dir_name = "test-dir-env-testing"
 
 New-Item -ItemType Directory -Force -Path "C:\$dir_name"
-Set-Content -Path "C:\$dir_name\ps-test.txt" -Value "localuser: $localpass, dcuser: $dcpass" -Force
+Set-Content -Path "C:\$dir_name\ps-test.txt" -Value "localuser from env: $localpass, dcuser from env: $dcpass" -Force
 
 Write-Output "The test directory has been created: $dir_name"
