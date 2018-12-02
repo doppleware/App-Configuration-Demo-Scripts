6dc1d493aafadd0bb6acdfad10d2345f7e8c3d90
katzy687 <katzy687@gmail.com>
2018-12-02 13:21:41 +0200

    updated

--- handle_cmd_args.ps1
+++ handle_cmd_args.ps1
@@ -4,3 +4,10 @@ param(
 )
 
 Write-Output "local password is: $localpass, dc password is: $dcpass"
+
+$dir_name = "test-dir"
+
+New-Item -ItemType Directory -Force -Path "C:\$dir_name"
+Set-Content -Path "C:\$dir_name\ps-test.txt" -Value "local password is: $localpass, dc password is: $dcpass" -Force
+
+Write-Output "The test directory has been created on C Drive: $dir_name"
