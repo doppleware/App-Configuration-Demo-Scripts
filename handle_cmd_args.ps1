7467e08d09387d9635409c369e198b9e450697bf
katzy687 <katzy687@gmail.com>
2018-12-02 14:24:29 +0200

    updated

--- handle_cmd_args.ps1
+++ handle_cmd_args.ps1
@@ -3,21 +3,6 @@ param(
   [string]$dcpass
 )
 
-# turning off local firewall, if you want to leave on you need to open up winrm ports 5985
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-
-# Set network to private (necessary for winrm to work)
-Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
-
-# allow winrm config
-Enable-PSRemoting -SkipNetworkProfileCheck -Force
-winrm set winrm/config/client/auth '@{Basic="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-
-# set execution policy to allow scripts to run
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-
 Write-Output "local password is: $localpass, dc password is: $dcpass"
 
 $dir_name = "test-dir"
